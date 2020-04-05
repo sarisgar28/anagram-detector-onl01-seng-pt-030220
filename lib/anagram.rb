db5367207274cb2b862a54f401de4369eaf68d12
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-04-05 18:19:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words 
+  end 
+  
+  def match(w)
+    w.downcase.split("").sort.join('')
+    words == w
+  end
+  
+  
+
+  
+
+    
+  
+  
+  
+  
+  
+end 
