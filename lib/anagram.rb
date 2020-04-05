0b60506ae885782994ce1c010f24c946f509226e
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-04-05 18:49:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words 
+  end 
+  
+  def match(word)
+    word.select do |w|
+    w.downcase.split("").sort == words.downcase.split("").sort
+    end 
+  end
+  
+  
+
+  
+
+    
+  
+  
+  
+  
+  
+end 
